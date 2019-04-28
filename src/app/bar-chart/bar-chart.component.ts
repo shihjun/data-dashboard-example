f43c8263395798ea841443d0fc0a949498d1d0f0
Ming Hao Chan <cmh114933@gmail.com>
2019-04-28 11:23:33 +0800

    completed

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import multiYearLaptopSales from '../../multiYearLaptopSales'
+
 
 @Component({
   selector: 'app-bar-chart',
@@ -6,9 +8,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bar-chart.component.css']
 })
 export class BarChartComponent implements OnInit {
+  chartType = "bar"
+  chartData = []
+  // [
+  //   { data: [],  label: "model"}
+  // ]
+  chartLabels = []
+
   constructor() { }
 
   ngOnInit() {
+    // multiYearLaptopSales : Transaction []
+
+    //   [{
+    //     "transactionDate": "Sun Feb 25 2018 19:41:51 GMT+0800 (Malaysia Time)",
+    //     "monthSold": 1,
+    //     "yearSold": 2018,
+    //     "model": "latitude",
+    //     "cost": 2500,
+    //     "salesPrice": 6000
+    // },]
+
+    let monthlyProfits = {}
+    // {
+    //   month: {
+    //     model: profit
+    //   }
+    // }
+    for( let transaction of multiYearLaptopSales){
+      if(!monthlyProfits[transaction.monthSold]){
+        monthlyProfits[transaction.monthSold] = {}
+      }
+      if(!monthlyProfits[transaction.monthSold][transaction.model]){
+        monthlyProfits[transaction.monthSold][transaction.model] = 0
+      }
+      const transactionProfit = transaction.salesPrice - transaction.cost
+      monthlyProfits[transaction.monthSold][transaction.model] += transactionProfit
+    }
+
+
+    let aggregateModelProfits = {}
+    // {
+    //   inspiron: [profits, profits, profits],
+    //   xps: [profits, profits, profits],
+    //   latitude: [profits, profits, profits]
+    // }
+    this.chartLabels = Object.keys(monthlyProfits)
+    for(let month of Object.keys(monthlyProfits)){
+      for(let model in monthlyProfits[month]){
+        if(!aggregateModelProfits[model]){
+          aggregateModelProfits[model] = []
+        }
+        aggregateModelProfits[model].push(monthlyProfits[month][model])
+      }
+    }
+
+    let dataset = []
+
+    for(let model in aggregateModelProfits){
+      dataset.push({
+        data: aggregateModelProfits[model],
+        label: model
+      })
+    }
+
+    this.chartData = dataset
   }
 
 }
--- src/app/pie-chart/pie-chart.component.ts
+++ src/app/pie-chart/pie-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import movieUserPreference from '../../movieUserPreference'
 
 @Component({
   selector: 'app-pie-chart',
@@ -6,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pie-chart.component.css']
 })
 export class PieChartComponent implements OnInit {
+  chartType = "pie"
+  chartData = []
+  // [
+  //  { data: [1,2,3,4]} 
+  // ]
+  chartLabels = []
+  // ["Avengers", "Frozen", "The Incredibles", "Justice League"] 
+
   constructor() { }
 
-  ngOnInit() {  }
+  ngOnInit() {  
+    let dataset = []
+    let movieLikeCount = {}
+    for( let preference of movieUserPreference){
+      if(!movieLikeCount[preference.movie]){
+        movieLikeCount[preference.movie] = 0
+      }
+      movieLikeCount[preference.movie] += 1
+    }
+    this.chartLabels = Object.keys(movieLikeCount)
+    for( let movie of Object.keys(movieLikeCount)){
+      dataset.push(movieLikeCount[movie])
+    }
+    this.chartData.push({
+      data: dataset
+    })
+  }
 
 }
 
